6834b452e034b01c2e2abfab7489a3bcb0a48eaf
Kornelije Sajler <xajler@gmail.com>
2012-10-15 19:13:09 +0200

    Created Account and Tenant full CRUD REST API Controllers.

---
+++ RestApiWeb/Controllers/AccountsController.cs
@@ -0,0 +1,84 @@
+﻿namespace RestApiWeb.Controllers
+{
+    using RestApiWeb.Context;
+    using RestApiWeb.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Web.Http;
+
+    public class AccountsController : ApiController
+    {
+        private readonly RestApiAzureContext _context;
+
+        public AccountsController()
+        {
+            this._context = new RestApiAzureContext();
+        }
+
+        public IEnumerable<Account> Get()
+        {
+            ICollection<Account> result = this._context.Accounts.ToList();
+            return result;
+        }
+
+        public Account Get(Guid id)
+        {
+            Account result = this._context.Accounts.SingleOrDefault(x => x.Id == id);
+            return result;
+        }
+
+        public Account Post(Account account)
+        {
+            try
+            {
+                account.Id = Guid.NewGuid();
+                this._context.Accounts.Add(account);
+                this._context.SaveChanges();
+                return account;
+            }
+            catch(Exception exception)
+            {
+                Debug.WriteLine(exception.Message + "\n\n" + exception.StackTrace);
+                return account;
+            }
+        }
+
+        public Account Put(Guid id, [FromBody]Account account)
+        {
+            try
+            {
+                Account result = this._context.Accounts.SingleOrDefault(x => x.Id == id);
+                result.Name = account.Name;
+                result.Email = account.Email;
+                result.IsActive = account.IsActive;
+
+                this._context.SaveChanges();
+
+                return result;
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine(exception.Message + "\n\n" + exception.StackTrace);
+                return account;
+            }
+        }
+
+        public bool Delete(Guid id)
+        {
+            try
+            {
+                Account account = this._context.Accounts.SingleOrDefault(x => x.Id == id);
+                this._context.Accounts.Remove(account);
+                this._context.SaveChanges();
+                return true;
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine(exception.Message + "\n\n" + exception.StackTrace);
+                return false;
+            }
+        }
+    }
+}
---
+++ RestApiWeb/Controllers/TenantsController.cs
@@ -0,0 +1,86 @@
+﻿namespace RestApiWeb.Controllers
+{
+    using RestApiWeb.Context;
+    using RestApiWeb.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Web.Http;
+
+    public class TenantsController : ApiController
+    {
+        private readonly RestApiAzureContext _context;
+
+        public TenantsController()
+        {
+            this._context = new RestApiAzureContext();
+        }
+
+        public IEnumerable<Tenant> Get()
+        {
+            ICollection<Tenant> result = this._context.Tenants.ToList();
+            return result;
+        }
+
+        public Tenant Get(Guid id)
+        {
+            Tenant result = this._context.Tenants.SingleOrDefault(x => x.Id == id);
+            return result;
+        }
+
+        public Tenant Post(Tenant tenant)
+        {
+            try
+            {
+                tenant.Id = Guid.NewGuid();
+                this._context.Tenants.Add(tenant);
+                this._context.SaveChanges();
+                return tenant;
+            }
+            catch(Exception exception)
+            {
+                Debug.WriteLine(exception.Message + "\n\n" + exception.StackTrace);
+                return tenant;
+            }
+        }
+
+        public Tenant Put(Guid id, [FromBody]Tenant tenant)
+        {
+            try
+            {
+                Tenant result = this._context.Tenants.SingleOrDefault(x => x.Id == id);
+                result.Code = tenant.Code;
+                result.Name = tenant.Name;
+                result.Description = tenant.Description;
+                result.AccountId = tenant.AccountId;
+                result.IsActive = tenant.IsActive;
+
+                this._context.SaveChanges();
+
+                return result;
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine(exception.Message + "\n\n" + exception.StackTrace);
+                return tenant;
+            }
+        }
+
+        public bool Delete(Guid id)
+        {
+            try
+            {
+                Tenant tenant = this._context.Tenants.SingleOrDefault(x => x.Id == id);
+                this._context.Tenants.Remove(tenant);
+                this._context.SaveChanges();
+                return true;
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine(exception.Message + "\n\n" + exception.StackTrace);
+                return false;
+            }
+        }
+    }
+}
