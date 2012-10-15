cdddf56734624342c646c899c369d536891cf4fc
Kornelije Sajler <xajler@gmail.com>
2012-10-15 15:20:54 +0200

    Created Models: Account and Tenant.

---
+++ RestApiWeb/Models/Tenant.cs
@@ -0,0 +1,21 @@
+﻿namespace RestApiWeb.Models
+{
+    using System;
+
+    public class Tenant
+    {
+        public Guid Id { get; set; }
+
+        public int Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public Guid AccountId { get; set; }
+
+        public Account Account { get; set; }
+    }
+}
