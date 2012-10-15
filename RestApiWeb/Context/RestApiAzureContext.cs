9994d211bfae7b46c7c929aa879f675abc1a764b
Kornelije Sajler <xajler@gmail.com>
2012-10-15 17:33:50 +0200

    Created RestApiAzure Entity Framework Context.

---
+++ RestApiWeb/Context/RestApiAzureContext.cs
@@ -0,0 +1,17 @@
+﻿namespace RestApiWeb.Context
+{
+    using RestApiWeb.Models;
+    using System.Data.Entity;
+
+    public class RestApiAzureContext : DbContext
+    {
+        public RestApiAzureContext()
+            : base("RestApiAzure")
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+
+        public DbSet<Tenant> Tenants { get; set; }
+    }
+}
